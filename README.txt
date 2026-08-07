03fbd62f9d81b38fa5b868fc78367c5a93469a73
cobbnut <tim.vanlaun@gmail.com>
2026-08-08 07:34:48 +1000

    First build

---
+++ README.txt
@@ -0,0 +1,15 @@
+BRUCE CONNECTIONS
+
+Files:
+  index.html       The game itself.
+  puzzle-data.js  The puzzle. Edit this for a new game.
+
+Hosting:
+  Upload both files to the same folder on any static web host.
+
+Puzzle format:
+  There must be exactly 4 groups and exactly 4 unique words/phrases per group.
+
+Test locally:
+  Double-click index.html. Because puzzle-data.js is loaded as a normal script,
+  the game works locally without a web server.
---
+++ puzzle-data.js
@@ -0,0 +1,36 @@
+/**
+ * Puzzle data for the charity Connections-style game.
+ *
+ * Edit only this file when you want to create a new puzzle.
+ *
+ * Each group needs:
+ *   - name: the explanation shown after the group is solved
+ *   - colour: one of "yellow", "green", "blue", "purple"
+ *   - words: exactly four entries
+ */
+window.PUZZLE_DATA = {
+    title: "Bruce's Connections",
+    subtitle: "Find four groups of four connected words",
+    groups: [
+        {
+            name: "THINGS THAT ARE RED",
+            colour: "yellow",
+            words: ["APPLE", "ROSE", "STOP SIGN", "RUBY"]
+        },
+        {
+            name: "KINDS OF DANCE",
+            colour: "green",
+            words: ["SALSA", "TANGO", "WALTZ", "RUMBA"]
+        },
+        {
+            name: "CAN FOLLOW 'BLUE'",
+            colour: "blue",
+            words: ["BIRD", "BELL", "MOON", "WHALE"]
+        },
+        {
+            name: "WORDS WITH SILENT FIRST LETTERS",
+            colour: "purple",
+            words: ["KNIFE", "GNOME", "PSALM", "WRIST"]
+        }
+    ]
+};
